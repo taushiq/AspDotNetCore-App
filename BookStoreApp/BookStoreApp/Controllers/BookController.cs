c32407fce51e38c8341d87d3d2b926c14a6f966d
Taushiq <taushiqawais@gmail.com>
2021-03-27 13:37:16 +0530

    Installed JQuery and Bootstrap

---
+++ BookStoreApp/BookStoreApp/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStoreApp.Models;
+using BookStoreApp.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        public ViewResult GetAllBooks()
+        {
+            var data = _bookRepository.GetAllBooks();
+            return View();
+        }
+
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBooks(string bookName, string authorName)
+        {
+            return _bookRepository.SearchBook(bookName, authorName);
+        }
+    }
+}
